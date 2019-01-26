2d76a265a4e81f8d11e042877c371a251ba9e7c2
Fritz Johnson <fritzj96@gmail.com>
2019-01-26 07:51:52 -0800

    added password hashing and user saving. Should be able to register now.

--- app/models/user.js
+++ app/models/user.js
@@ -26,3 +26,14 @@ module.exports.getUserByUsername = (username, callback) => {
     const query = { username };
     User.findOne(query, callback);
 }
+
+module.exports.addUser = (newUser, callback) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) throw err;
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
