5194f9b1a52fc5c953be75c85edce9c5e0deb6d8
Fritz Johnson <fritzj96@gmail.com>
2019-01-26 07:48:48 -0800

    added packages + updated the register route

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', (req, res, next) => {
-    res.send('Register');
+    const { username, email, password} = req.body;
+    let newUser = new User({
+        username,
+        email,
+        password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err) {
+            res.json({ success: false, msg: 'Failed To Register User'});
+        }
+        else {
+            res.json({ success: true, msg: 'User Registered Successfully' });
+        }
+    });
 });
 
 router.post('/authenticate', (req, res, next) => {
