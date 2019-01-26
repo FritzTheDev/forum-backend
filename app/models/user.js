3d34c3ce94a97837d542d4cdb9bf582153572561
Fritz Johnson <fritzj96@gmail.com>
2019-01-26 07:42:35 -0800

    added User Schema & Model + a couple query methods

---
+++ app/models/user.js
@@ -0,0 +1,28 @@
+const mongoose, { Schema } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = (id, callback) => {
+    User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = (username, callback) => {
+    const query = { username };
+    User.findOne(query, callback);
+}
